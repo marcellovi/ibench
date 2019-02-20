b0f4cd4147ca6bdc89026c55be8e41d6e6e919e2
Sotero Junior <junior_abreu_8@hotmail.com>
2019-02-20 14:26:12 -0300

    retorna o usuario para tela de detalhe do produto quando estava deslogado

--- _site/local/app/Http/Controllers/Auth/LoginController.php
+++ _site/local/app/Http/Controllers/Auth/LoginController.php
@@ -16,244 +16,204 @@ use Auth;
 use Illuminate\Support\Facades\Validator;
 use Input;
 use Redirect;
+use Storage;
 
 
 class LoginController extends Controller
 {
-/*
-|--------------------------------------------------------------------------
-| Login Controller
-|--------------------------------------------------------------------------
-|
-| This controller handles authenticating users for the application and
-| redirecting them to your home screen. The controller uses a trait
-| to conveniently provide its functionality to your applications.
-|
-*/
+	/*
+	|--------------------------------------------------------------------------
+	| Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles authenticating users for the application and
+	| redirecting them to your home screen. The controller uses a trait
+	| to conveniently provide its functionality to your applications.
+	|
+	*/
 
-use AuthenticatesUsers;
-
-
-protected function authenticated(Request $request, $user)
-{
-if(auth()->check() && auth()->user()->id == 1){
-            
-			return redirect('/admin');
-        }
-		else
-		{
-			return redirect('/dashboard');
-		}
-
-        
-}
-
-/********* SOCIAL LOGIN ********/
-public function redirectToProvider($provider)
-{
-       return Socialite::driver($provider)->scopes(['email'])->redirect(); 
-         /*return Socialite::driver('google')->scopes(['profile','email'])->redirect();*/
-}
+	use AuthenticatesUsers;
 
 
-public function clean($string) 
+	protected function authenticated(Request $request, $user)
 	{
-    
-     $string = preg_replace("/[^\p{L}\/_|+ -]/ui","",$string);
+		if(auth()->check() && auth()->user()->id == 1)
+		{      
+			return redirect('/admin');
+	  }else{
+	  	return redirect('/dashboard');
+	  }  
+	}
 
-    
-    $string = preg_replace("/[\/_|+ -]+/", '-', $string);
+	/********* SOCIAL LOGIN ********/
+	public function redirectToProvider($provider)
+	{
+		return Socialite::driver($provider)->scopes(['email'])->redirect(); 
+	  /*return Socialite::driver('google')->scopes(['profile','email'])->redirect();*/
+	}
 
-    
-    $string =  trim($string,'-');
+	public function clean($string) 
+	{
+		$string = preg_replace("/[^\p{L}\/_|+ -]/ui","",$string);
 
-    return mb_strtolower($string);
+		$string = preg_replace("/[\/_|+ -]+/", '-', $string);
+
+		$string =  trim($string,'-');
+
+		return mb_strtolower($string);
 	}  
-   
-   
- public function handleProviderCallback($provider)
-    {
-        $user = Socialite::driver($provider)->user();
-        $authUser = $this->findOrCreateUser($user, $provider);
-        Auth::login($authUser, true);
-        /*return redirect($this->redirectToProvider);*/
-       /* return redirect()->action('IndexController@index');*/
-       return redirect('dashboard');
-
-    }
-	
-	
-	
-	
-	
- public function findOrCreateUser($user, $provider)
-    {
-        $authUser = User::where('provider_id', $user->id)->first();
-        if ($authUser) {
-            return $authUser;
-        }
-        return User::create([
-            'name'     => $user->name,
-			'post_slug' => $this->clean($user->name),
-            'email'    => $user->email,
-			'admin' => 2,
-            'provider' => $provider,
-            'provider_id' => $user->id
-        ]);
-    }	
-
-/**************** SOCIAL LOGIN **********/
-
-
-
-
-public function username()
-{
-    return 'username';
-}
-
-
-protected function credentials(Request $request)
-{
-    $usernameInput = trim($request->{$this->username()});
-    $usernameColumn = filter_var($usernameInput, FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
-
-    return [$usernameColumn => $usernameInput, 'password' => $request->password];
-	
-	/* return [$usernameColumn => $usernameInput, 'password' => $request->password, 'active' => 1]; */
-}
-
-
-
-
-protected function login(Request $request)
-{
-	
-
-	$validator = Validator::make($request->all(), [
-            'username' => 'required|max:255',
-            'password' => 'required',
-			
+	   
+	   
+	public function handleProviderCallback($provider)
+	{
+	  $user = Socialite::driver($provider)->user();
+	  $authUser = $this->findOrCreateUser($user, $provider);
+	  Auth::login($authUser, true);
+	  /*return redirect($this->redirectToProvider);*/
+	 	/* return redirect()->action('IndexController@index');*/
+	 return redirect('dashboard');
+	}	
 		
-        ]);
+	public function findOrCreateUser($user, $provider)
+	{
+	  $authUser = User::where('provider_id', $user->id)->first();
+	  if ($authUser) 
+	  {
+	  	return $authUser;
+	  }
+	  	return User::create([
+	            'name'     => $user->name,
+							'post_slug' => $this->clean($user->name),
+	            'email'    => $user->email,
+							'admin' => 2,
+	            'provider' => $provider,
+	            'provider_id' => $user->id
+	        	]);
+	}	
 
-        $input = $request->all();
-		 
-        
-   if ($validator->passes()) 
-   {
-	
-		   
-		   $auth = false;
-    
-	
-	
-		$usernameInput = trim(Input::get('username'));
-		$usernameColumn = filter_var($usernameInput, FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
+	/**************** SOCIAL LOGIN **********/
+	public function username()
+	{
+	    return 'username';
+	}
 
-		if (Auth::attempt(array($usernameColumn => $usernameInput, 'password' => Input::get('password') ))) 
-		{
-			$auth = true; // Success
-			
-			$editprofile = DB::table('users')
-		               ->where('id', '=', auth()->user()->id)
-	                   ->get();
-    
-		   if (auth()->user()->confirmation == 0 && auth()->user()->admin!=1) 
-		   {
-            auth()->logout();
-			
-			return back()->with('get_error', $editprofile[0]->email);
 
-            
-           } 
-		   else
-           {
-		       if(Auth::user()->admin == 1)
-				{
-					return redirect('/admin');
-					
-				}
-                else
-				{	
-                                $id= auth()->user()->id;
-                                /* Marcello :: Limpando o carrinho ao fazer o login */
-                                DB::delete('delete from product_orders where user_id="'.$id.'" and purchase_token ="'.'" and '
-                                        . 'payment_token ="'.'" and payment_type ="'.'" and payment_status ="'.'"');			
+	protected function credentials(Request $request)
+	{
+	  $usernameInput = trim($request->{$this->username()});
+	  $usernameColumn = filter_var($usernameInput, FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
 
-                                return redirect('/dashboard');
-				}
-					
+	  return [$usernameColumn => $usernameInput, 'password' => $request->password];
+		
+		/* return [$usernameColumn => $usernameInput, 'password' => $request->password, 'active' => 1]; */
+	}
+
+
+	protected function login(Request $request)
+	{
+		
+		$validator =	Validator::make($request->all(), [
+	            			'username' => 'required|max:255',
+	            			'password' => 'required',
+	        				]);
+
+	  $input = $request->all();
+
+	  if ($validator->passes()) 
+	  {
+
+	  	$auth = false;
+			$usernameInput = trim(Input::get('username'));
+			$usernameColumn = filter_var($usernameInput, FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
+
+			if (Auth::attempt(array($usernameColumn => $usernameInput, 'password' => Input::get('password') ))) 
+			{
+
+				$auth = true; // Success
 				
-		   }		
-				
-				
-		}
-		
-		else
-		{
-			   
-			   return back()->with('error', 'Detalhes de login inv&aacute;lidos');
-		}
-		
-		   
-		
-		}
-		else
-		{
+				$editprofile = DB::table('users')
+			               		->where('id', '=', auth()->user()->id)
+		                  	->get();
+		    if (auth()->user()->confirmation == 0 && auth()->user()->admin!=1) 
+			  {
+			  	auth()->logout();
+			  	return back()->with('get_error', $editprofile[0]->email);
+			  }else{
+			  	
+			  	if(Auth::user()->admin == 1)
+					{
+
+						return redirect('/admin');
+
+					}else{	
+
+						$id= auth()->user()->id;
+	          /* Marcello :: Limpando o carrinho ao fazer o login */
+	          DB::delete('delete from product_orders where user_id="'.$id.'" and purchase_token ="'.'" and '
+	                                        . 'payment_token ="'.'" and payment_type ="'.'" and payment_status ="'.'"');		
+	          //Storage::put('log.txt', $input['url_prev']);
+	          /* Verifica se o params com URL Previous é de algum detalhar de produto e retorna para pagina quando logar */
+	          if (strpos($input['url_prev'], 'product') !== false) 
+	          {
+	          	return redirect($input['url_prev']);
+						}else{ 
+							return redirect('/dashboard');
+						}
+					}
+
+				}		
+
+			}else{
+				return back()->with('error', 'Detalhes de login inv&aacute;lidos');
+			}
+
+		}else{
 			return back()->with('error', 'Detalhes de login inv&aacute;lidos');
 		}
-		
-		
-		
+	        
+	}
 
-        
-}
-
-
-
-
-
-
-
-
- protected function sendFailedLoginResponse(Request $request)
-    {
-        $errors = [$this->username() => trans('auth.failed')];
-        // Load user from database
-        $user = DB::table('users')
-				->where('name', $request->{$this->username()})->first();
-        
-        if ($user && \Hash::check($request->password, $user->password) && $user->admin != 1) {
-            $errors = [$this->username() => 'Your account is not active.'];
-        }
-        if ($request->expectsJson()) {
-            return response()->json($errors, 422);
-        }
-        /*return redirect()->back()
-            ->withInput($request->only($this->username(), 'remember'))
-            ->withErrors($errors);*/
-			return back()->with('error', 'Detalhes de login inv&aacute;lidos');
-    }
+	protected function sendFailedLoginResponse(Request $request)
+	{
+	  $errors = [$this->username() => trans('auth.failed')];
+	  // Load user from database
+	  $user = DB::table('users')
+								->where('name', $request->{$this->username()})->first();
+	        
+	  if ($user && \Hash::check($request->password, $user->password) && $user->admin != 1) 
+	  {
+	    $errors = [$this->username() => 'Your account is not active.'];
+	  }
+	  
+	  if ($request->expectsJson()) 
+	  {
+	  	return response()->json($errors, 422);
+	  }
+	    /*return redirect()->back()
+	            ->withInput($request->only($this->username(), 'remember'))
+	            ->withErrors($errors);*/
+		return back()->with('error', 'Detalhes de login inv&aacute;lidos');
+	}
 
 
 
-/**
- * Where to redirect users after login.
- *
- * @var string
- */
-//protected $redirectTo = '/admin';
+	/**
+	 * Where to redirect users after login.
+	 *
+	 * @var string
+	 */
+	//protected $redirectTo = '/admin';
 
-/**
- * Create a new controller instance.
- *
- * @return void
- */
-public function __construct()
-{
-    $this->middleware('guest', ['except' => 'logout']);
-}
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+	  $this->middleware('guest', ['except' => 'logout']);
+	}
+
 }
 
 
--- _site/local/resources/views/auth/login.blade.php
+++ _site/local/resources/views/auth/login.blade.php
@@ -2,12 +2,8 @@
 
 @section('content')
 
-	
-
 <?php $url = URL::to("/"); ?>
 
-
-     
 <div class="breadcrumb">
 	<div class="container-fluid">
 		<div class="breadcrumb-inner">
@@ -19,190 +15,143 @@
 	</div>
 </div>
 
-
-
-
-
-
-
-
-
-    
-        
-       <div class="body-content">
+<div class="body-content">
 	<div class="container-fluid">
 		<div class="my-wishlist-page_new">
-        
-        <div class="row">
-        <div class="col-md-6"><div class="heading-title" style="border-bottom:none !important;">Login</div></div>
-                <div class="col-md-6 text-right"></div>
-        </div>
-        <div class="height50 clearfix"></div>
-        
-        <div class="col-md-3"></div>
-        
+
+      <div class="row">
         <div class="col-md-6">
-        
+          <div class="heading-title" style="border-bottom:none !important;">Login</div>
+        </div>
+        <div class="col-md-6 text-right"></div>
+      </div>
+
+      <div class="height50 clearfix"></div>
+      <div class="col-md-3"></div>
+
+      <div class="col-md-6">
         <div class="">
-	
-	
-	 @if(Session::has('success'))
+          @if(Session::has('success'))
+            <div class="alert alert-success">
+              {{ Session::get('success') }}
+            </div>
+          @endif
 
-	    <div class="alert alert-success">
+          @if(Session::has('error'))
+            <div class="alert alert-danger">
+              {{ Session::get('error') }}
+            </div>
+          @endif
+        </div>
 
-	      {{ Session::get('success') }}
+        <div class="panel panel-default ">
+          <div class="panel-heading">Login</div>
+          <div class="panel-body">
+            <form class="register-form" role="form" method="POST" action="{{ route('login') }}">
+              
+              <input type="hidden" name="_token" value="{{ csrf_token() }}">
+              
+              <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email" class="col-md-4">Usu&aacute;rio / Email</label>
+                <div class="col-md-8">
+                  <input id="username" type="text" class="form-control unicase-form-control" name="username" value="{{ old('email') }}" required autofocus>
 
-	    </div>
-
-	@endif
-
-
-	
-	
- 	
-	
-	
-        
-        @if(Session::has('error'))
-
-	    <div class="alert alert-danger">
-
-	      {{ Session::get('error') }}
-
-	    </div>
-
-	@endif
-    </div>
-        
-        
-            <div class="panel panel-default ">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="register-form" role="form" method="POST" action="{{ route('login') }}">
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-
-                   
-
-
-
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4">Usu&aacute;rio / Email</label>
-
-                            <div class="col-md-8">
-                                <input id="username" type="text" class="form-control unicase-form-control" name="username" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-<div class="height10 clearfix"></div>
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4">@lang('languages.password')</label>
-
-                            <div class="col-md-8">
-                                <input id="password" type="password" class="form-control unicase-form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label class="para black">
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Lembrar Senha
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                               
-                                <input name="" type="submit" value="Login" class="btn-upper btn btn-primary">
-
-                                <a class="btn btn-link para gold" href="{{ route('forgot-password') }}">
-                                    Esqueceu sua Senha?
-                                </a>
-                            </div>
-                        </div>
-                      <?php /*?><div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                            <a href="{{ url('/login/facebook') }}"><i class="fa fa-facebook-official" aria-hidden="true"></i> Facebook</a>
-        <a href="{{ url('/login/twitter') }}"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a>
-        <a href="{{ url('/login/github') }}"><i class="fa fa-github" aria-hidden="true"></i> Github</a>
-   <a href="{{ url('/login/google') }}" class="">google-plus</i></a>
-                             
-                             
-                             <a href="<?php echo $url;?>/login/facebook" class="">facebook</i></a>
-<a href="<?php echo $url;?>/login/twitter" class="">twitter</i></a>
-<a href="<?php echo $url;?>/login/google" class="">google-plus</i></a>
-<a href="<?php echo $url;?>/login/linkedin" class="">linkedin</i></a>
-<a href="<?php echo $url;?>/login/github" class="">github</i></a>
-</div>
-                        </div><?php */?> 
-                        
-                        <div class="height10 clearfix"></div>
-                        <div class="form-group">
-                            
-                            <div class="col-md-1"></div>
-                            <div class="col-md-10" align="center">
-                            
-                            <div class="ffleft" style="margin-right:5px;">
-  <!-- FB Marcello                              
-                            <a href="{{ url('/login/facebook') }}">
-
-<img src="<?php echo $url;?>/local/images/facebook_btn.png" border="0" alt="facebook login" class="img-responsive"  />
-
-</a>
--->
-</div>
-        
-       <div class="ffleft">
-           
-<!-- Google Marcello 
-   <a href="{{ url('/login/google') }}" class="">
-
-<img src="<?php echo $url;?>/local/images/google_btn.png" border="0" alt="google plus login" class="img-responsive" />
-</a>
--->
-</div>
-</div>
-                            
-
-                     <div class="col-md-1"></div>   
-                                       
-                      </form>
+                  <input type="hidden" name="url_prev" value="<?php echo url()->previous(); ?>">
+                  @if ($errors->has('email'))
+                    <span class="help-block">
+                      <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                  @endif
                 </div>
-            </div>
-            
-            </div>
-            
-            <div class="col-md-3"></div>
-            
-            
-            
+              </div>
+
+              <div class="height10 clearfix"></div>
+
+              <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                <label for="password" class="col-md-4">@lang('languages.password')</label>
+                <div class="col-md-8">
+                  <input id="password" type="password" class="form-control unicase-form-control" name="password" required>
+                  @if ($errors->has('password'))
+                    <span class="help-block">
+                      <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                  @endif
+                </div>
+              </div>
+
+              <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                  <div class="checkbox">
+                    <label class="para black">
+                      <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Lembrar Senha
+                    </label>
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <div class="col-md-8 col-md-offset-4">
+                  <input name="" type="submit" value="Login" class="btn-upper btn btn-primary">
+                  <a class="btn btn-link para gold" href="{{ route('forgot-password') }}">
+                    Esqueceu sua Senha?
+                  </a>
+                </div>
+              </div>
+              
+              <?php /*?><div class="form-group">
+                          <div class="col-md-6 col-md-offset-4">
+                            <a href="{{ url('/login/facebook') }}"><i class="fa fa-facebook-official" aria-hidden="true"></i> Facebook</a>
+                            <a href="{{ url('/login/twitter') }}"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a>
+                            <a href="{{ url('/login/github') }}"><i class="fa fa-github" aria-hidden="true"></i> Github</a>
+                            <a href="{{ url('/login/google') }}" class="">google-plus</i></a>  
+                            <a href="<?php echo $url;?>/login/facebook" class="">facebook</i></a>
+                            <a href="<?php echo $url;?>/login/twitter" class="">twitter</i></a>
+                            <a href="<?php echo $url;?>/login/google" class="">google-plus</i></a>
+                            <a href="<?php echo $url;?>/login/linkedin" class="">linkedin</i></a>
+                            <a href="<?php echo $url;?>/login/github" class="">github</i></a>
+                          </div>
+                        </div><?php */
+              ?> 
+
+              <div class="height10 clearfix"></div>
+              <div class="form-group">
+                <div class="col-md-1"></div>
+                <div class="col-md-10" align="center">
+                  <div class="ffleft" style="margin-right:5px;">
+                    <!-- FB Marcello                              
+                        <a href="{{ url('/login/facebook') }}">
+                        <img src="<?php echo $url;?>/local/images/facebook_btn.png" border="0" alt="facebook login" class="img-responsive"  />
+                      </a>
+                    -->    
+                  </div>
+                  <div class="ffleft">
+                  <!-- Google Marcello 
+                    <a href="{{ url('/login/google') }}" class="">
+                      <img src="<?php echo $url;?>/local/images/google_btn.png" border="0" alt="google plus login" class="img-responsive" />
+                    </a>
+                  -->
+                  </div>
+                </div>
+
+              <div class="col-md-1"></div>  
+
+            </form>
+
+          </div>
         </div>
-        
-        </div>
-   
+
+      </div>
+            
+      <div class="col-md-3"></div>
+
     </div>
+
+  </div>
+
+</div>
 
 <div class="height50"></div>
 
-
-
-	
-	
 	
 @include('footer')
 @endsection

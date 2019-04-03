af06d5b46eab01242e274f1c1a072218dfb66e94
marcellovi <marcello.strategy@gmail.com>
2019-04-03 13:05:28 -0300

    Incluido campo de Numero de Produtos Inativos. Acertos na Formatacao.

--- _site/local/resources/views/admin/vendors.blade.php
+++ _site/local/resources/views/admin/vendors.blade.php
@@ -1,173 +1,187 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-   
-   @include('admin.title')
-    
-    @include('admin.style')
-    
-     @include('admin.table-style')
-  </head>
+    <head>
+        @include('admin.title')
+        @include('admin.style')
+        @include('admin.table-style')
+    </head>
 
-  <body>
-  
-   @include('admin.top')
+    <body>
+        @include('admin.top')
+        @include('admin.menu')
+        <?php $url = URL::to("/"); ?>
 
-@include('admin.menu')
-<?php $url = URL::to("/"); ?>
+        <div id="content">
+            <div id="content-header">
+                <div id="breadcrumb">  </div>
+                <h1>Fornecedores</h1>
+            </div>
+            
+            <div class="container-fluid"><hr>
+                <div class="row-fluid">
+                    <div class="span12">
+                        @if(Session::has('error'))
+                        <div class="alert alert-error">
+                            <button class="close" data-dismiss="alert"><i class="icon-off"></i></button>
+                            {{ Session::get('error') }}
+                        </div>
+                        @endif
+
+                        @if(Session::has('success'))
+                        <div class="alert alert-success">
+                            <button class="close" data-dismiss="alert"><i class="icon-off"></i></button>
+                            {{ Session::get('success') }}
+                        </div>
+                        @endif
+
+                        <form action="{{ route('admin.users') }}" method="post">
+
+                            {{ csrf_field() }}
+                            <div align="right">
+
+                                <?php if (config('global.demosite') == "yes") { ?>
+
+                                    <a href="#" class="btn btn-danger btndisable">Delete All</a>  <span class="disabletxt">( <?php echo config('global.demotxt'); ?> )</span>
+                                <?php } else { ?>
+                                    <input type="submit" value="Deletar Todos" class="btn btn-danger" id="checkBtn" onClick="return confirm('Tem certeza que quer deletar?');">
+                                <?php } ?>
 
 
+                                <?php if (config('global.demosite') == "yes") { ?>
+                                    <span class="disabletxt">( <?php echo config('global.demotxt'); ?> )</span> <a href="#" class="btn btn-primary btndisable">Adicionar Usuario</a> 
+                                <?php } else { ?>
+                                    <a href="<?php echo $url; ?>/admin/adduser" class="btn btn-primary">Adicionar Usuario</a>
+                                <?php } ?>
+                            </div>
+                            <div class="widget-box">
 
 
-<div id="content">
-  <div id="content-header">
-    <div id="breadcrumb">  </div>
-    <h1>Fornecedores</h1>
-  </div>
-  <div class="container-fluid"><hr>
-    <div class="row-fluid">
-      <div class="span12">
-      @if(Session::has('error'))
-      <div class="alert alert-error">
-              <button class="close" data-dismiss="alert"><i class="icon-off"></i></button>
-              {{ Session::get('error') }}
-              </div>
-      @endif
-      
-      @if(Session::has('success'))
+                                <div class="widget-title"> <span class="icon"><i class="icon-th"></i></span>
+                                    <h5>Fornecedores</h5>
+                                </div>
 
-	           
-        <div class="alert alert-success">
-              <button class="close" data-dismiss="alert"><i class="icon-off"></i></button>
-               {{ Session::get('success') }}
-              </div>
+                                <div class="widget-content nopadding">
+                                    <table class="table table-bordered data-table" id="datatable-responsive">
+                                        <thead>
+                                            <tr>
+                                                <th data-orderable="false"><input type="checkbox" id="selectAll" class="main">
+                                                </th>
+                                                <th>Sno</th>
+                                                <th>Imagem</th>
+                                                <th>Usuario</th>
+                                                <th>Email</th>
+                                                <th>CNPJ</th>
+                                                <th>Telefone</th>
+                                                <th>Ganhos</th>
+                                                <th>N. Prod Inativo</th>
+                                                <th>Status</th>
+                                                <th>Wirecard</th>
+                                                <th>A&ccedil;&atilde;o</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            if (!empty($users_cnt)) {
+                                                $i = 1;
+                                                foreach ($users as $user) {
 
-	@endif
+                                                    $sta = $user->admin;
+                                                    if ($sta == 1) {
+                                                        $viewst = "Admin";
+                                                    } else if ($sta == 2) {
+                                                        $viewst = "Vendor";
+                                                    } else if ($sta == 0) {
+                                                        $viewst = "Customer";
+                                                    }
+                                                    ?>
 
 
-<form action="{{ route('admin.users') }}" method="post">
-                 
-                 {{ csrf_field() }}
-                 <div align="right">
-                     
-                  <?php if(config('global.demosite')=="yes"){ ?>
-				
-				   <a href="#" class="btn btn-danger btndisable">Delete All</a>  <span class="disabletxt">( <?php echo config('global.demotxt');?> )</span>
-				  <?php } else { ?>
-				   <input type="submit" value="Deletar Todos" class="btn btn-danger" id="checkBtn" onClick="return confirm('Tem certeza que quer deletar?');">
-				  <?php } ?>
-                  
-                  
-				  <?php if(config('global.demosite')=="yes"){?>
-				  <span class="disabletxt">( <?php echo config('global.demotxt');?> )</span> <a href="#" class="btn btn-primary btndisable">Adicionar Usuario</a> 
-				  <?php } else { ?>
-				  <a href="<?php echo $url;?>/admin/adduser" class="btn btn-primary">Adicionar Usuario</a>
-				  <?php } ?>
-                 </div>
-<div class="widget-box">
-           
+                                                    <tr class="gradeX">
 
-          <div class="widget-title"> <span class="icon"><i class="icon-th"></i></span>
-            <h5>Fornecedores</h5>
-          </div>
-          
-          
-          <div class="widget-content nopadding">
-         
-            <table class="table table-bordered data-table" id="datatable-responsive">
-              <thead>
-                <tr>
-                  <th data-orderable="false"><input type="checkbox" id="selectAll" class="main">
-          </th>
-                  <th>Sno</th>
-						  <th>Imagem</th>
-                          <th>Usuario</th>
-                          <th>Email</th>
-                          <th>CNPJ</th>
-						  <th>Telefone</th>
-                          <th>Ganhos</th>
-                          <th>Status</th>
-                          <th>Wirecard</th>
-                          <th>A&ccedil;&atilde;o</th>
-                </tr>
-              </thead>
-              <tbody>
-              <?php if(!empty($users_cnt)){
-					  $i=1;
-					  foreach ($users as $user) { $sta=$user->admin; if($sta==1){ $viewst="Admin"; } else if($sta==2) { $viewst="Vendor"; } else if($sta==0) { $viewst="Customer"; }?>
-                
-                
-                <tr class="gradeX">
-                        
-                         <td align="center"><input type="checkbox" name="userid[]" value="<?php echo $user->id;?>"/></td>
-                        
-						 <td><?php echo $i;?></td>
-						 <?php 
-					   $userphoto="/media/";
-						$path ='/local/images'.$userphoto.$user->photo;
-						if($user->photo!=""){
-						?>
-						 <td><img src="<?php echo $url.$path;?>" class="thumb" width="70"></td>
-						 <?php } else { ?>
-						  <td><img src="<?php echo $url.'/local/images/nophoto.jpg';?>" class="thumb" width="70"></td>
-						 <?php } ?>
-                          <td><?php echo $user->name;?></td>
-                          <td><?php echo $user->email;?></td>
-                          <td><?php echo $user->cpf_cnpj;?></td>
-			  <td><?php echo $user->phone;?></td>                          
-                          
-                          <?php if($user->provider=="") { $logintype = "normal"; } else { $logintype = $user->provider; } ?>
-                                <td><?php echo $user->earning;?> <?php echo $setts[0]->site_currency;?></td>
-                                <td><?php if($user->delete_status == ""){ echo "Liberado"; }else{ echo "Bloqueado";} ?></td>
-                                <td><?php if($user->wirecard_app_data != ""){ echo "Conectado"; }else{ echo "N/A";} ?></td>
-                          
-				<td>
-                                    <?php if($user->provider==""){?>
-                                        <?php if(config('global.demosite')=="yes"){?>
-                                            <a href="#" class="btn btn-success btndisable">Editar</a>  <span class="disabletxt">( <?php echo config('global.demotxt');?> )</span>
-                                        <?php } else { ?>						  
-                                            <a href="<?php echo $url;?>/admin/edituser/{{ $user->id }}" class="btn btn-success">Editar</a>
-                                        <?php } ?>
-                                    <?php } ?>        
-                   
-				    <?php if(config('global.demosite')=="yes"){?>
-                                        <a href="#" class="btn btn-danger btndisable">Deletar</a>  <span class="disabletxt">( <?php echo config('global.demotxt');?> )</span>
-                                    <?php } else { ?>						  
-                                            @if($sta!=1)
-                                                   <a href="<?php echo $url;?>/admin/users/{{ $user->id }}" class="btn btn-danger" onClick="return confirm('Tem certeza que quer deletar?')">Deletar</a> 
-                                                    <?php if($user->delete_status == ""){ ?> 
-                                                        <a href="<?php echo $url;?>/admin/authorizeseller/{{ $user->id }}/0" class="btn btn-primary" onClick="return confirm('Tem certeza que quer Bloquear o fornecedor?')">Bloquear</a>
-                                                    <?php }else{ ?>
-                                                        <a href="<?php echo $url;?>/admin/authorizeseller/{{ $user->id }}/1" class="btn btn-primary" onClick="return confirm('Tem certeza que quer liberar o fornecedor?')">Liberar</a>
-                                                    <?php } ?>
-                                            @endif
-						  
-				    <?php } ?>
-				</td>
-                        </tr>
-                
-                <?php $i++; } } ?>
-                                
-              </tbody>
-            </table>
-           
-          </div>
-          
+                                                        <td align="center"><input type="checkbox" name="userid[]" value="<?php echo $user->id; ?>"/></td>
+
+                                                        <td><?php echo $i; ?></td>
+                                                        <?php
+                                                        $userphoto = "/media/";
+                                                        $path = '/local/images' . $userphoto . $user->photo;
+                                                        if ($user->photo != "") {
+                                                            ?>
+                                                            <td><img src="<?php echo $url . $path; ?>" class="thumb" width="70"></td>
+        <?php } else { ?>
+                                                            <td><img src="<?php echo $url . '/local/images/nophoto.jpg'; ?>" class="thumb" width="70"></td>
+                                                        <?php } ?>
+                                                        <td><?php echo $user->name; ?></td>
+                                                        <td><?php echo $user->email; ?></td>
+                                                        <td><?php echo $user->cpf_cnpj; ?></td>
+                                                        <td><?php echo $user->phone; ?></td>                          
+
+                                                            <?php if ($user->provider == "") {
+                                                                $logintype = "normal";
+                                                            } else {
+                                                                $logintype = $user->provider;
+                                                            } ?>
+                                                        <td><?php echo $user->earning; ?> <?php echo $setts[0]->site_currency; ?></td>
+                                                        <td><?php
+                                                            //print_r($prod_cnt_inactive); exit();
+                                                            $found = false;
+                                                            foreach ($prod_cnt_inactive as $user_prod_inactive) {
+                                                                if ($user->id == $user_prod_inactive->user_id) {
+                                                                    echo $user_prod_inactive->qtd_prod_inactive;
+                                                                    $found = true;
+                                                                }
+                                                            }if (!$found) {
+                                                                echo "0";
+                                                            }
+                                                            ?></td>
+                                                        <td><?php if ($user->delete_status == "") {
+                                                        echo "Liberado";
+                                                    } else if ($user->delete_status == "inactive") {
+                                                        echo "Inativo";
+                                                    } else {
+                                                        echo "Bloqueado";
+                                                    } ?></td>
+                                                        <td><?php if ($user->wirecard_app_data != "") {
+                                                        echo "Conectado";
+                                                    } else {
+                                                        echo "N/A";
+                                                    } ?></td>
+
+                                                        <td>
+        <?php if ($user->provider == "") { ?>
+            <?php if (config('global.demosite') == "yes") { ?>
+                  <a href="#" class="btn btn-success btndisable">Editar</a>  <span class="disabletxt">( <?php echo config('global.demotxt'); ?> )</span>
+            <?php } else { ?>						  
+                  <a href="<?php echo $url; ?>/admin/edituser/{{ $user->id }}" class="btn btn-success">Editar</a>
+            <?php } ?>
+        <?php } ?>        
+
+        <?php if (config('global.demosite') == "yes") { ?>
+                 <a href="#" class="btn btn-danger btndisable">Deletar</a>  <span class="disabletxt">( <?php echo config('global.demotxt'); ?> )</span>
+        <?php } else { ?>						  
+                @if($sta!=1)
+                 <a href="<?php echo $url; ?>/admin/users/{{ $user->id }}" class="btn btn-danger" onClick="return confirm('Tem certeza que quer deletar?')">Deletar</a> 
+            <?php if ($user->delete_status == "") { ?> 
+                  <a href="<?php echo $url; ?>/admin/authorizeseller/{{ $user->id }}/0" class="btn btn-primary" onClick="return confirm('Tem certeza que quer Bloquear o fornecedor?')">Bloquear</a>
+            <?php } else { ?>
+                  <a href="<?php echo $url; ?>/admin/authorizeseller/{{ $user->id }}/1" class="btn btn-primary" onClick="return confirm('Tem certeza que quer liberar o fornecedor?')">Liberar</a>
+            <?php } ?>
+                 @endif
+
+        <?php } ?>
+                                                        </td>
+                                                    </tr>
+        <?php $i++;    }} ?>
+                                        </tbody>
+                                    </table>
+
+                                </div>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-   </form>
-  
-  
-  
-   
-		 </div>
-         </div>
-         </div>
-         
-         
-         </div>
-		 
-
-    @include('admin.footer')
-	
-  </body>
+        @include('admin.footer')
+    </body>
 </html>

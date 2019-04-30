9602afb7476c0750dfb1a265d2e96b7a3849a88e
marcellovi <marcello.strategy@gmail.com>
2019-04-30 11:38:42 -0300

    Acerto para Aceitar Caracteres Especiais Ajustes na formtacao do Codigo

--- _site/local/resources/views/admin/add_attribute_type.blade.php
+++ _site/local/resources/views/admin/add_attribute_type.blade.php
@@ -1,21 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-   
-  
-     @include('admin.title')
-    @include('admin.style')
-	
-    
+    @include('admin.title')
+    @include('admin.style')    
   </head>
-
-  <body>
+<body>
   @include('admin.top')
-
-@include('admin.menu') 
-  
-  
-  
+  @include('admin.menu') 
+    
   <div id="content">
   <div id="content-header">
     <div id="breadcrumb">  </div>
@@ -31,80 +23,59 @@
               </div>
       @endif
       
-      @if(Session::has('success'))
-
-	           
+        @if(Session::has('success'))	           
         <div class="alert alert-success">
               <button class="close" data-dismiss="alert"><i class="icon-off"></i></button>
                {{ Session::get('success') }}
               </div>
-
-	@endif
+        @endif
 
         <div class="widget-box">
           <div class="widget-title"> <span class="icon"> <i class="icon-info-sign"></i> </span>
             <h5>Adicionar Tipo</h5>
           </div>
-          <div class="widget-content nopadding">
-          
+          <div class="widget-content nopadding">          
               
               <?php $url = URL::to("/"); ?>   
-                   <form class="form-horizontal form-label-left" role="form" method="POST" action="{{ route('admin.add_attribute_type') }}"  enctype="multipart/form-data" id="formID">
-                     {{ csrf_field() }} 
-                     
-                    
+              <form class="form-horizontal form-label-left" role="form" method="POST" action="{{ route('admin.add_attribute_type') }}"  enctype="multipart/form-data" accept-charset="utf-8" id="formID">
+                  {{ csrf_field() }} 
                      
               <div class="control-group">
                 <label class="control-label">Nome</label>
-                <div class="controls">
-                 
+                <div class="controls">                 
                                 
                    <input id="name" class="validate[required] span8"  name="name" value="" type="text">
 						   @if ($errors->has('name'))
                                     <span class="help-block" style="color:red;">
                                         <strong>That type is already exists</strong>
                                     </span>
-                                @endif              
-                  
+                                @endif            
                 </div>
-              </div>
-              
+              </div>              
               
               <div class="control-group">
                 <label class="control-label">Incluir na Pesquisa?</label> <!-- Marcello Search for category and product -->
-                <div class="controls">
-                 
-                                
+                <div class="controls">               
                    <input id="enable_search" class=""  name="enable_search" value="1" type="checkbox">
-						                
-                  
                 </div>
               </div>
-              
-              
-              
-              
-              
-              
-              <div class="control-group">
-              
+                         
+            <div class="control-group">              
 					
               <?php $url = URL::to("/"); ?>
               <div class="form-actions">
                         <div class="span8">
                          
-                          <a href="<?php echo $url;?>/admin/attribute_type" class="btn btn-primary">Cancelar</a>
-                        
+                          <a href="<?php echo $url;?>/admin/attribute_type" class="btn btn-primary">Cancelar</a>                        
                        
-						  <?php if(config('global.demosite')=="yes"){?><button type="button" class="btn btn-success btndisable">Submit</button> 
-								<span class="disabletxt">( <?php echo config('global.demotxt');?> )</span><?php } else { ?>
-						  
-                           
-                           <button id="send" type="submit" class="btn btn-success">Submit</button>
-								<?php } ?>
+                          <?php if (config('global.demosite') == "yes") { ?><button type="button" class="btn btn-success btndisable">Submit</button> 
+                              <span class="disabletxt">( <?php echo config('global.demotxt'); ?> )</span><?php } else { ?>
+
+
+                              <button id="send" type="submit" class="btn btn-success">Submit</button>
+                          <?php } ?>
                         </div>
-                        </div>
-              
+                        </div>              
             </form>
           </div>
         </div>
@@ -113,14 +84,7 @@
     
   </div>
 </div>
-
-
-
-</div>
-  
-  
-  
- @include('admin.footer')
-	
-  </body>
+</div>  
+ @include('admin.footer')	
+</body>
 </html>

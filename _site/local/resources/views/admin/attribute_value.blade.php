aef8dc0b8f21b9f98c3fda1b98273f46f0ddef14
marcellovi <marcello.strategy@gmail.com>
2019-04-30 11:47:22 -0300

    Acerto para Aceitar Caracteres Especiais Ajustes na formtacao do Codigo

--- _site/local/resources/views/admin/attribute_value.blade.php
+++ _site/local/resources/views/admin/attribute_value.blade.php
@@ -3,27 +3,18 @@ use Illuminate\Support\Facades\Route;
 $currentPaths= Route::getFacadeRoot()->current()->uri();	
 $url = URL::to("/");
 $log_id = Auth::user()->id;
-				 ?>
+?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-   
+  <head>   
    @include('admin.title')
-    
-    @include('admin.style')
-    @include('admin.table-style')
-    
+   @include('admin.style')
+   @include('admin.table-style')    
   </head>
-
-  <body>
-  
-  
+<body> 
   @include('admin.top')
-
-@include('admin.menu')
-<?php $url = URL::to("/"); ?>
-  
-  
+  @include('admin.menu')
+<?php $url = URL::to("/"); ?>  
   
   <div id="content">
   <div id="content-header">
@@ -40,165 +31,112 @@ $log_id = Auth::user()->id;
               </div>
       @endif
       
-      @if(Session::has('success'))
-
-	           
+      @if(Session::has('success'))	           
         <div class="alert alert-success">
               <button class="close" data-dismiss="alert"><i class="icon-off"></i></button>
                {{ Session::get('success') }}
               </div>
-
 	@endif
 
-
-
-                 <div align="right">
-                  
-                  
-				   <?php if(config('global.demosite')=="yes"){?>
-				  <span class="disabletxt">( <?php echo config('global.demotxt');?> )</span> <a href="#" class="btn btn-primary btndisable">Adicionar Valor</a> 
-				  <?php } else { ?>
-				  <a href="<?php echo $url;?>/admin/add_attribute_value" class="btn btn-primary">Adicionar Valor</a>
-				  <?php } ?>
-                 </div>
-<div class="widget-box">
-           
-
-          <div class="widget-title"> <span class="icon"><i class="icon-th"></i></span>
-            <h5>Valor do Atributo</h5>
-          </div>
-          
-          
-          <div class="widget-content nopadding">
-         
-            <table class="table table-bordered data-table" id="datatable-responsive">
-              <thead>
-                <tr>
-                                           
-                          
-                        
-                          <th>Sno</th>
-						  
-                          <th>Tipo Nome</th>
-                          
-                          <th>Valor</th>
-                          
-                          <th>Status</th>
-                         
-                          <th>A&ccedil;&atilde;o</th>
-                         
-                         </tr>
-                         
-                         
-                         
-                         
-              </thead>
-              <tbody>
-             <?php 
-					  $i=1;
-					  foreach ($attribute_value as $value) { 
-					  
-					  $attribute_type_cnt = DB::table('product_attribute_type')
-		                                ->where('delete_status','=','')
-										->where('attr_id','=',$value->attr_id)
-					                    ->count();
-					  if(!empty($attribute_type_cnt))
-					  {					
-					  $attribute_type = DB::table('product_attribute_type')
-		                                ->where('delete_status','=','')
-										->where('attr_id','=',$value->attr_id)
-					                    ->get();
-										
-					  $att_name = $attribute_type[0]->attr_name;
-					  }
-					  else
-					  {
-					  $att_name = "";
-					  }										
-					  ?>
-                      
-                      
-                        <tr class="gradeX">
-                       
-                        
-						 <td><?php echo $i;?></td>
-						 
-                          <td><?php echo $att_name;?></td>
-                          
-                          <td><?php echo $value->attr_value;?></td>
-                          
-                          <?php if($value->status==1){ $status_txt = "Active"; $clrs = "green"; } else { $status_txt = "Inactive"; $clrs = "red"; }?>
-                          
-						  <td style="color:<?php echo $clrs;?>; font-weight:bold;"><?php echo $status_txt;?></td>
-                          
-						  <td>
-                          <?php if($log_id==$value->user_id){?>
-                          <?php if(config('global.demosite')=="yes"){?>
-				    <a href="#" class="btn btn-success btndisable">Active</a>  <span class="disabletxt">( <?php echo config('global.demotxt');?> )</span>
-				  <?php } else { ?>
-						  
-						  <a href="<?php echo $url;?>/admin/attribute_value/action/{{ $value->value_id }}/1" class="btn btn-success">Active</a>
-						  
-				  <?php } ?>
-                  
-                  
-                  <?php if(config('global.demosite')=="yes"){?>
-				    <a href="#" class="btn btn-danger btndisable">Inactive</a>  <span class="disabletxt">( <?php echo config('global.demotxt');?> )</span>
-				  <?php } else { ?>
-						  
-						  <a href="<?php echo $url;?>/admin/attribute_value/action/{{ $value->value_id }}/0" class="btn btn-danger">Inactive</a>
-						  
-				  <?php } ?>
-                  
-                  
-                          
-                          
-						   <?php if(config('global.demosite')=="yes"){?>
-						  <a href="#" class="btn btn-success btndisable">Editar</a>  <span class="disabletxt">( <?php echo config('global.demotxt');?> )</span>
-				  <?php } else { ?>
-						  <a href="<?php echo $url;?>/admin/edit_attribute_value/{{ $value->value_id }}" class="btn btn-success">Editar</a>
-						  <?php } ?>
-                          
-                          <?php } ?>
-                          
-				   <?php if(config('global.demosite')=="yes"){?>
-				   <a href="#" class="btn btn-danger btndisable">Deletar</a>  <span class="disabletxt">( <?php echo config('global.demotxt');?> )</span>
-				  <?php } else { ?>
-						 <a href="<?php echo $url;?>/admin/attribute_value/{{ $value->value_id }}" class="btn btn-danger" onClick="return confirm('Tem certeza que deseja deletar?')">Deletar</a>
-						 
-					 <?php } ?>
-						 </td>
-                         
-                         
-                        </tr>
-                        
-                    
-                <?php $i++;} ?>
-                                
-              </tbody>
-            </table>
-           
-          </div>
-          
+        <div align="right">
+            <?php if (config('global.demosite') == "yes") { ?>
+                <span class="disabletxt">( <?php echo config('global.demotxt'); ?> )</span> <a href="#" class="btn btn-primary btndisable">Adicionar Valor</a> 
+            <?php } else { ?>
+                <a href="<?php echo $url; ?>/admin/add_attribute_value" class="btn btn-primary">Adicionar Valor</a>
+            <?php } ?>
         </div>
-  
-  
-  
-  
-   
-		 </div>
-         </div>
-         </div>
-         
-         
-         </div>
-  
-  
-  
-  
-      
+<div class="widget-box">           
 
+    <div class="widget-title"> <span class="icon"><i class="icon-th"></i></span>
+        <h5>Valor do Atributo</h5>
+    </div>          
+          
+    <div class="widget-content nopadding">
+
+        <table class="table table-bordered data-table" id="datatable-responsive">
+            <thead>
+                <tr>  
+                    <th>Sno</th>						  
+                    <th>Tipo Nome</th>                          
+                    <th>Valor</th>                          
+                    <th>Status</th>                         
+                    <th>A&ccedil;&atilde;o</th>                         
+                </tr>  
+
+            </thead>
+            <tbody>
+                <?php
+                $i = 1;
+                foreach ($attribute_value as $value) {
+
+                    $attribute_type_cnt = DB::table('product_attribute_type')
+                            ->where('delete_status', '=', '')
+                            ->where('attr_id', '=', $value->attr_id)
+                            ->count();
+                    if (!empty($attribute_type_cnt)) {
+                        $attribute_type = DB::table('product_attribute_type')
+                                ->where('delete_status', '=', '')
+                                ->where('attr_id', '=', $value->attr_id)
+                                ->get();
+
+                        $att_name = $attribute_type[0]->attr_name;
+                    } else {
+                        $att_name = "";
+                    }
+                    ?>
+                
+                    <tr class="gradeX">
+
+                        <td><?php echo $i; ?></td>
+                        <td><?php echo utf8_decode($att_name); ?></td>
+                        <td><?php echo utf8_decode($value->attr_value); ?></td>
+
+    <?php if ($value->status == 1) {
+        $status_txt = "Active";
+        $clrs = "green";
+    } else {
+        $status_txt = "Inactive";
+        $clrs = "red";
+    } ?>
+                        <td style="color:<?php echo $clrs; ?>; font-weight:bold;"><?php echo $status_txt; ?></td>
+                        <td>
+                            <?php if ($log_id == $value->user_id) { ?>
+                                    <?php if (config('global.demosite') == "yes") { ?>
+                                        <a href="#" class="btn btn-success btndisable">Active</a>  <span class="disabletxt">( <?php echo config('global.demotxt'); ?> )</span>
+                                    <?php } else { ?>
+                                        <a href="<?php echo $url; ?>/admin/attribute_value/action/{{ $value->value_id }}/1" class="btn btn-success">Active</a>
+                                    <?php } ?>
+
+                                    <?php if (config('global.demosite') == "yes") { ?>
+                                        <a href="#" class="btn btn-danger btndisable">Inactive</a>  <span class="disabletxt">( <?php echo config('global.demotxt'); ?> )</span>
+                                    <?php } else { ?>
+                                        <a href="<?php echo $url; ?>/admin/attribute_value/action/{{ $value->value_id }}/0" class="btn btn-danger">Inactive</a>
+                                    <?php } ?>
+
+                                    <?php if (config('global.demosite') == "yes") { ?>
+                                        <a href="#" class="btn btn-success btndisable">Editar</a>  <span class="disabletxt">( <?php echo config('global.demotxt'); ?> )</span>
+                                    <?php } else { ?>
+                                        <a href="<?php echo $url; ?>/admin/edit_attribute_value/{{ $value->value_id }}" class="btn btn-success">Editar</a>
+                                    <?php } ?>
+
+                                <?php } ?>
+
+                            <?php if (config('global.demosite') == "yes") { ?>
+                                <a href="#" class="btn btn-danger btndisable">Deletar</a>  <span class="disabletxt">( <?php echo config('global.demotxt'); ?> )</span>
+                            <?php } else { ?>
+                                <a href="<?php echo $url; ?>/admin/attribute_value/{{ $value->value_id }}" class="btn btn-danger" onClick="return confirm('Tem certeza que deseja deletar?')">Deletar</a>
+                            <?php } ?>
+                        </td>
+                    </tr>
+    <?php $i++;    } ?>
+            </tbody>
+        </table>
+    </div>          
+        </div>  
+        </div>
+        </div>
+        </div>     
+        </div>  
      @include('admin.footer') 
-	
   </body>
 </html>

73239b40ea08e54d589ec8e8e35f98734f0ff78d
marcellovi <marcello.strategy@gmail.com>
2019-06-25 18:24:49 -0300

    Acerto de UTF

--- _site/local/resources/views/admin/add-blog.blade.php
+++ _site/local/resources/views/admin/add-blog.blade.php
@@ -1,21 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-   
-  
-     @include('admin.title')
-    @include('admin.style')
-	
-    
+  <head>  
+    @include('admin.title')
+    @include('admin.style')  
   </head>
-
   <body>
    @include('admin.top')
 <!--close-top-serch-->
 <!--sidebar-menu-->
 @include('admin.menu')
-
-
 <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=cae5v8ohqq2y45yurx2yqba3ng4rqukel679jhibsfg3gk4r"></script>
 <script> tinymce.init({ selector:'textarea' });</script>
 
@@ -34,14 +27,11 @@
               </div>
       @endif
       
-      @if(Session::has('success'))
-
-	           
+      @if(Session::has('success'))	           
         <div class="alert alert-success">
               <button class="close" data-dismiss="alert"><i class="icon-off"></i></button>
                {{ Session::get('success') }}
               </div>
-
 	@endif
 
         <div class="widget-box">
@@ -58,16 +48,13 @@
                 <label class="control-label">Post Title</label>
                 <div class="controls">
                  <input id="name" class="validate[required] text-input span8"  name="post_title" value="" type="text">
-						   @if ($errors->has('post_title'))
+				@if ($errors->has('post_title'))
                                     <span class="help-block" style="color:red;">
                                         <strong>That post title is already exists</strong>
                                     </span>
-                                @endif
-                  
+                                @endif                  
                 </div>
-              </div>
-              
-              
+              </div>                 
               
               <div class="control-group">
                 <label class="control-label">Description</label>
@@ -83,55 +70,49 @@
                 <div class="controls">
                 
                    <select class="validate[required] span8"  name="media_type"  onChange="showDiv(this)">
-						  <option value=""></option>
-						  <?php 
-						  $mediatype=array("image","mp3","video");
-						  foreach($mediatype as $media){?>
-						  <option value="<?php echo $media;?>"><?php echo $media;?></option>
-						  <?php } ?>
-						  </select>
+                          <option value=""></option>
+                          <?php 
+                          $mediatype=array("image","mp3","video");
+                          foreach($mediatype as $media){?>
+                          <option value="<?php echo $media;?>"><?php echo $media;?></option>
+                          <?php } ?>
+                          </select>
                 </div>
-              </div>
-              
+              </div>              
               
               <div class="control-group" id="mediaurl">
                 <label class="control-label">Youtube or Vimeo Url</label>
                 <div class="controls">
                  <input id="video_url" class="validate[required] span8"  name="video_url" value="{{ old('video_url') }}" type="text">
 						   <br/>( Example : https://www.youtube.com/watch?v=2MpUj-Aua48  OR https://vimeo.com/56282283 )
-                        
-                </div>
-              </div>
-              
-             
+                  </div>
+              </div>               
               
                <div class="control-group" id="mediamp3">
                 <label class="control-label">Mp3 Upload</label>
                 <div class="controls">
                  <input type="file" id="audio_file" name="audio_file" class="validate[required] span8">
 						  
-						  @if ($errors->has('audio_file'))
+				@if ($errors->has('audio_file'))
                                     <span class="help-block" style="color:red;">
                                         <strong>{{ $errors->first('audio_file') }}</strong>
                                     </span>
                                 @endif
                 </div>
-              </div>
-              
+              </div>              
               
                <div class="control-group" id="mediaimg">
                 <label class="control-label">Image</label>
                 <div class="controls">
                 <input type="file" id="photo" name="photo" class="validate[required] span8">
 						  
-						  @if ($errors->has('photo'))
+				@if ($errors->has('photo'))
                                     <span class="help-block" style="color:red;">
                                         <strong>{{ $errors->first('photo') }}</strong>
                                     </span>
                                 @endif
                 </div>
-              </div>
-              
+              </div>              
               
                <div class="control-group" id="mediaurl">
                 <label class="control-label">Tags</label>
@@ -140,40 +121,25 @@
                    <input id="tags" class="span8"  name="tags"  type="text" data-role="tagsinput">
 					<p>(Example : blog post,latest post,popular blog,trending,social media )</p>   
                 </div>
-              </div>
-              
-              
-              
+              </div>                
 					
               <?php $url = URL::to("/"); ?>
               <div class="form-actions">
                         <div class="span8">
                         <a href="<?php echo $url;?>/admin/blog" class="btn btn-primary">Cancel</a>
-						  <?php if(config('global.demosite')=="yes"){?><button type="button" class="btn btn-success btndisable">Submit</button> 
-								<span class="disabletxt">( <?php echo config('global.demotxt');?> )</span><?php } else { ?>
-						  
-                            <button id="send" type="submit" class="btn btn-success">Submit</button>
-								<?php } ?>
-                        </div>
-              
+			<?php if(config('global.demosite')=="yes"){?><button type="button" class="btn btn-success btndisable">Submit</button> 
+			<span class="disabletxt">( <?php echo config('global.demotxt');?> )</span><?php } else { ?>						  
+                        <button id="send" type="submit" class="btn btn-success">Submit</button>
+			<?php } ?>
+                        </div>              
             </form>
           </div>
         </div>
       </div>
-    </div>
-    
+    </div>    
   </div>
 </div>
-
-
-
-</div>
-
-
-
-
-
-    
-	 @include('admin.footer')
-  </body>
+</div>   
+@include('admin.footer')
+</body>
 </html>

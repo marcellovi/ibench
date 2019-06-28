3c57781dce8e0b7b669732a815741efbd9b227ca
Sotero Junior <junior_abreu_8@hotmail.com>
2019-06-28 16:38:38 -0300

    test

--- _site/local/resources/views/shop/02.blade.php
+++
@@ -1,5 +0,0 @@
- <li> <a href="<?php echo $url; ?>">@lang('languages.home')</a></li>
- <li class='active'>@lang('languages.shop')</li>
- <?php if (!empty($search_txt)) { ?>
-   <li class='active'><?php echo utf8_decode($search_txt); ?></li>
- <?php } ?>
--- _site/local/resources/views/shop/10.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="col col-sm-10 col-md-10 text-right">
-  <i class="fa fa-flask"></i>&nbsp;&nbsp;<?php if (isset($category)) {
-                                            echo 'Total de ' . $viewcount . '  Produtos';
-                                          } ?>
-
-
-</div>

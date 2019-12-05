c062ab6f997c2c1eea7e6b5eec99022f14102cea
marce <marce@DESKTOP-GBOIQLK>
2019-12-04 23:04:27 -0200

    Acerto para quando for clicar no pesquisar com campo de texto em branco

--- route.php
+++ route.php
@@ -3,6 +3,8 @@
 if(isset($_POST['search_text']) && !empty($_POST['search_text'])){
     $txt = $_POST['search_text'];
     echo "<script>window.location.href = 'https://www.ibench.com.br/_site/searched/$txt';</script>";
+}else if(isset($_POST['search_text']) && empty($_POST['search_text'])){
+     echo "<script>window.location.href = 'https://www.ibench.com.br/_site/shop';</script>";
 }
 
 

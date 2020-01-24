ba0a6f674cc2295a5bbb69247f9bdc3cfeca31d0
marce <marce@DESKTOP-GBOIQLK>
2020-01-24 19:21:37 -0200

    Atualizacao do arquivo Language.
    adicionado ibench_now header para as paginas lang 
    / faça pedido(place_order)

--- _site/local/resources/lang/pt/languages.php
+++ _site/local/resources/lang/pt/languages.php
@@ -5,10 +5,10 @@
 
 return [
 
-     
-	 
+
+
 	 /* common */
-	 
+
 	 "home" => "Home",
 	 "some_problem" => "Desculpe! Ocorreu alguns problemas na sua inser&ccedil;&atilde;o.",
 	 "name" => "Nome",
@@ -19,7 +19,7 @@ return [
 
 	"subject" => "Subject",
 	 "cancellation_refund_request" => "Cancellation & Refund Request",
-	 
+
 	 "product_url" => "Product Url",
 	 "profile_url" => "Profile Url",
 	 "vendor_details" => "Vendor Details",
@@ -31,16 +31,16 @@ return [
 
 
     /* 404 page */
-	
+
     "page_not_found" => "Oops, Pagina N&atilde;o Encontrada !",
 	"page_sorry" => "Desculpa, a pagina n&atilde;o foi encontada no servidor! Favor retorne",
-    
+
 	/* 404 page */
-	
-	
-	
+
+
+
 	/* attribute type & value */
-	
+
 	"add_attribute_type" => "Adicionar Atributo Typo",
 	"view_my_attribute_type" => "Visualizar Meu ATT. Tipo",
 	"add_attribute_value" => "Adicionar Atributo Valor",
@@ -54,18 +54,18 @@ return [
 	"my_attribute_value" => "Meu Atributo Valor",
 	"add_value" => "Adicionar Valor",
 	"attribute_value" => "Atributo Valor",
-	
+
 	"edit_attribute_type" => "Editar ATT Tipo",
 	"edit_attribute_value" => "Editar ATT Valor",
-	
+
 	/* attribute type & value */
-	
-	
-	
-	
+
+
+
+
 
 	/* product */
-	
+
 	"add_product" => "Adicionar Produto",
 	"product_name" => "Nome Produto",
 	"url_slug" => "Url Link",
@@ -83,23 +83,23 @@ return [
 	"upload_zip_file" => "Upload Arquivo e formato .Zip (1024Max)",
 	"tag_separate" => "Tags (separar tags com ,)",
 	"edit_product" => "Editar Produto",
-	
+
 	"product_import_export" => "Produtos importa / exporta",
-	
+
 	"goto_import_export" => "Importar Produtos em Massa",
-	
-	
+
+
 	"print_btn" => "Imprimir",
-	
+
 	"import_file" => "Importa Arquivo",
         "import_model_file" => "Importar Planilha",
-	
+
 	/* product */
-	
-	
-	
+
+
+
 	/* BLog */
-	
+
 	"blog" => "Blog",
 	"comments" => "Coment&aacute;rios",
 	"leave_a_reply" => "Deixe sua resposta",
@@ -117,23 +117,23 @@ return [
 	"previous" => "anterior",
 	"next" => "pr&oacute;ximo",
 	"message" => "Mensagem",
-	
+
 	/* Blog */
-	
-	
-	
-	
+
+
+
+
 	/* cancel page */
 	"paypal_cancel" => "PayPal Transa&ccedil;&atilde;o Cancelada",
 	"your_paypal_cancel" => "Sua PayPal Transa&ccedil;&atilde;o foi cancelada.",
-	
-	
+
+
 	/* cancel page */
-	
-	
-	
+
+
+
 	/* cart page */
-	
+
 	"cart" => "Carrinho",
 	"my_cart" => "Meu Carrinho",
 	"remove" => "Remover",
@@ -151,19 +151,19 @@ return [
 	"shipping_charge" => "Taxa Envio",
 	"proceed_to_checkout" => "Concluir o Checkout",
 	"your_cart_is_empty" => "Seu carrinho esta vazio",
-	
+
 	/* cart page */
-	
-	
-	
-	
+
+
+
+
 	/* checkout page */
-	
+
 	"checkout" => "Checkout",
 	"billing_details" => "Informa&ccedil;&otilde;es para Cobran&ccedil;a",
 	"shipping_details" => "Detalhes de Envio",
 	"first_name" => "Primeiro Nome",
-	"last_name" => "Sobre Nome",
+	"last_name" => "Sobrenome",
 	"company_name" => "Nome Empresa",
 	"email_address" => "Email",
 	"phone" => "Telefone",
@@ -176,23 +176,23 @@ return [
 	"order_notes" => "Informa&ccedil;&otilde;es para Emiss&atilde;o da Nota Fiscal",
 	"total_order" => "Total do Pedido",
 	"select_payment_method" => "Selecione o Metodo de Pagamento",
-	"place_order" => "Fa&ccedil;a o Pedido",
-	
+	"place_order" => "Seguir para pagamento",
+
 	/* checkout page */
-	
-	
-	
-	
+
+
+
+
 	/* comment mail */
-	
+
 	"comment_received" => "Coment&aacute;rio Recebido",
-	
+
 	/* comment mail */
-	
-	
-	
+
+
+
 	/* compare */
-	
+
 	"product_comparison" => "Produto Comparado",
 	"product" => "Produto",
 	"rating" => "Avalia&ccedil;&atilde;o",
@@ -201,26 +201,26 @@ return [
 	"out_of_stock" => "Sem Estoque",
 	"add_to_cart" => "Adicionar ao Carrinho",
 	"add_to_wishlist" => "Adicionar a Lista de desejos",
-	
+
 	/* compare */
-	
-	
-	
+
+
+
 	/* contact us page */
-	
+
 	"contact_us" => "Fale Conosco",
 	"email" => "Email",
 	"phone_no" => "Telefone/Celular",
 	"send_message" => "Enviar Mensagem",
 	"information" => "Informa&ccedil;&atilde;o",
-	
-	
+
+
 	/* contact us page */
-	
-	
-	
+
+
+
 	/* dashboard */
-	
+
 	"my_dashboard" => "Meu Dashboard",
 	"dashboard" => "Dashboard",
 	"username" => "Usu&aacute;rio",
@@ -237,27 +237,27 @@ return [
 	"city" => "Cidade",
 	"state" => "Estado",
 	"update" => "Atualizar",
-	
+
 	/* dashboard */
-	
-	
-	
+
+
+
 	/* footer */
-	
+
 	"quick_links" => "Links R&aacute;pidos",
 	"all_vendors" => "Todos Fornecedores",
 	"wish_list" => "Lista de desejos",
 	"follow_us" => "Nos Siga",
 	"copyright" => "Copyright",
 	"all_right_reserved" => "Todos os direitos reservados",
-	
+
 	/* footer */
-	
-	
-	
-	
+
+
+
+
 	/* forgot email */
-	
+
         "img_logo_email" => "logo_email.jpg",
 	"forgot_password" => "Redefinir Senha",
 	"hello" => "Ol&aacute; Usu&aacute;rio,",
@@ -267,14 +267,14 @@ return [
 	"regards" => "Abra&ccedil;o,",
 	"admin" => "Equipe iBench",
 	"send_password" => "Link para Resetar Senha",
-	
+
 	/* forgot email */
-	
-	
-	
-	
+
+
+
+
 	/* header */
-	
+
 	"english" => "Ingles",
 	"spanish" => "Espanhol",
 	"french" => "Frances",
@@ -296,31 +296,32 @@ return [
 	"all" => "Todos",
 	"pages" => "empresa",
 	"vendors" => "Fornecedores",
-	
+	"ibench_now" => "N&atilde;o encontrou algo?",
+
 	/* header */
-	
-	
-	
-	
-	
+
+
+
+
+
 	/* home page */
-	
+
 	"new_products" => "Novos Produtos ",
 	"new" => "novo",
 	"login_user" => "Login Usu&aacute;rio",
 	"featured_products" => "Produtos em Destaque",
 	"latest_blog" => "Novidades no Blog",
 	"latest_products" => "Produtos Recentes",
-	
-	
-	
+
+
+
 	/* home page */
-	
-	
-	
-	
+
+
+
+
 	/* my balance */
-	
+
 	"minimum_withdraw" => "Valor Minimo para Retirar",
 	"available_balance" => "Balan&ccedil;o Disponivel",
 	"cleared_funds" => "Fundo disponivel para retirada",
@@ -341,14 +342,14 @@ return [
 	"bank_info" => "Banco Info",
 	"status" => "Status",
 	"completed_withdrawal" => "Retirada Completada",
-	
+
 	/* my balance */
 
-	
-	
-	
+
+
+
 	/* my orders */
-	
+
 	"my_orders" => "Minhas Vendas",
 	"purchase_id" => "N&ordm; do Pedido",
 	"attributes" => "Atributos",
@@ -357,13 +358,13 @@ return [
         "buyer_name" => "Nome do Comprador",
 	"view_more" => "Visualize Mais",
 	"qty" => "Qtd",
-	
+
 	/* my orders */
-	
-	
-	
+
+
+
 	/* my products */
-	
+
 	"my_products" => "Meus Produtos",
 	"featured" => "Destaque",
 	"approved" => "Aprovado",
@@ -372,36 +373,36 @@ return [
 	"payment" => "Pagamento",
 	"payment_date" => "Data Pagamento",
 	"payment_status" => "Status do Pagamento",
-	
+
 	/* my products */
-	
-	
-	
+
+
+
 	/* my wishlist */
-	
+
 	"my_wishlist" => "Minha Lista de desejos",
 	"payment_confirmation" => "Confirma&ccedil;&atilde;o de Pagamento",
-	
-	
+
+
 	/* my wishlist */
-	
-	
-	
+
+
+
 	/* product */
-	
+
 	"shop" => "Produtos",
 	"buy_now" => "COMPRE AGORA",
 	"review" => "Avalia&ccedil;&atilde;o",
 	"tags" => "TAGS",
 	"customer_reviews" => "Avalia&ccedil;&atilde;o dos Clientes",
 	"related_products" => "Produtos Relacionados",
-	
+
 	/* product */
-	
-	
-	
+
+
+
 	/* profile */
-	
+
 	"my_profile" => "Meu Perfil",
 	"vendor" => "Fornecedor",
 	"customer" => "Cliente",
@@ -410,32 +411,32 @@ return [
 	"contact_information" => "Informa&ccedil;&atilde;o de Contato",
 	"contact_form" => "Formul&aacute;rio de Contato",
 	"your_phone_number" => "Seu tel./cel. n&uacute;mero",
-	
+
 	/* profile */
-	
-	
-	
+
+
+
 	/* reset password */
 	"your_profile_password" => "Sua senha de acesso ao iBench Market foi redefinida com sucesso.",
 	"to_sign" => "Para Registar, v&aacute; para",
 	"enter_the_following" => "e digite o usu&aacute;rio e senha :",
 	"username_email" => "Usu&aacute;rio / Email",
-	
-	
+
+
 	/* reset password */
-	
-	
-	
+
+
+
 	/* seller contact mail */
-	
+
 	"contact_vendor" => "Entre em contato - Fornecedor",
 	"contact_details" => "Detalhes do contato",
-	
+
 	/* seller contact mail */
-	
-	
+
+
 	/* shop */
-	
+
 	"shop_by" => "liste por",
 	"all_category" => "Todas Categorias",
 	"under" => "Abaixo",
@@ -455,38 +456,38 @@ return [
 	"payment_id" => "C&oacute;digo de Pagamento",
 	"cancellation_refund" => "Cancellation & Refund",
 	"cancellation_request" => "Click to refund request",
-	
-	
+
+
 	/* shop */
-	
-	
-	
+
+
+
 	/* tags */
-	
+
 	"tagged_with" => "Marcado com",
-	
+
 	/* tags */
-	
-	
+
+
 	/* thankyou */
-	
+
 	"thank_you" => "Obrigado",
 	"oops_error" => "Humm Erro Encontrado",
 	"thank_you_message" => "Seu pedido foi enviado. Ap&oacute;s a confirma&ccedil;&atilde;o do pagamento seu pedido ser&aacute; confirmado. Obrigado.",
 	"your_order_id" => "ID do seu pedido",
 	/* thankyou */
-	
-	
-	
+
+
+
 	/* vendor */
-	
+
 	"view_profile" => "Visualize Perfil",
-	
+
 	/* vendor */
-	
-	
+
+
 	/* view orders */
-	
+
 	"view_orders" => "Visualize Pedidos",
 	"order_details" => "Detalhes do Pedido",
 	"stripe_txn_id" => "Stripe Txn Id",
@@ -494,56 +495,56 @@ return [
 	"shipping_price" => "Custo do Envio",
 	"back_to_my_orders" => "VOLTAR PARA Meus Pedidos",
 	"customer_details" => "Detalhes do Cliente",
-	
+
 	"other_details" => "Outros Detalhes",
 	"other_notes" => "Outras Anota&ccedil;&otilde;es",
-	
+
 	/* view orders */
-	
-	
-	
+
+
+
 	/* view shopping */
-	
+
 	"back_to_my_shopping" => "Voltar Para Minhas Compras",
 	"rating_review" => "Avalia&ccedil;&atilde;o & Review",
 	"download_file" => "Download Arquivo",
 	"click_rating_review" => "Click Avalia&ccedil;&atilde;o & Review",
 	"write_a_review" => "Escreva sua avalia&ccedil;&atilde;o",
-	
+
 	/* view shopping */
-	
-	
-	
+
+
+
 	/* withdraw */
-	
+
 	"withdrawal_request" => "Pedido de Retirada",
 	"withdraw_type" => "Tipo de Retirada",
-	
+
 	/* withdraw */
-    
+
 
 
     	/* Marcello - Style.blade.php */
         "option_type_prod1" => "fisico",
         "option_type_prod2" => "digital",
         "option_type_prod3" => "externo",
-        
+
 	/* Style.blade.php */
-    
+
         /* Marcello - Tooltips */
         "tooledit" => "Editar",
         "tooldelete" => "Excluir",
         "add_cart" => "Adicionar ao Carrinho",
         "wishlist" => "Lista de desejos",
         "compare" => "compare",
-    
+
         /* Marcello - Funcoes Basicas */
         "btnedit" => "Editar",
         "btndelete" => "Deletar",
         "btneditall" => "Editar Todos",
         "btndeleteall" => "Deletar Todos",
         "back" => "Voltar",
-    
+
         /* Marcello - Mensagens do Sistema */
         "msg_delete" => "Deseja realmente deletar?", // Are you sure you want to delete this?
         "msg_wait_approval" => "Aguardando Aprovacao", // Waiting For Approval
@@ -551,38 +552,38 @@ return [
         "msg_view_more" => "Veja Mais",
         "msg_confirm_pwd" => "Confirmar Senha",
         "msg_captcha_invalid" => "Campo Obrigatorio",   // The captcha field is required or invalid
-    
+
         /* Marcello - WalletID & CustomerID */
         "cpf_cnpj" => "CPF / CNPJ",
         "customer_id" => "CustomerID",
-    
-        /* Marcello - WalletID & CustomerID */ 
+
+        /* Marcello - WalletID & CustomerID */
         // Notes about your order, e.g. special notes for delivery.
         "title_order_notes" => "Anota&ccedil;&otilde;es da sua ordem, informa&ccedil;&otilde;es de envio.",
-    
+
         /* Marcello - WalletID & CustomerID */
         "type_cash_on_delivery" => "Pagamento na Entrega",
         "btn_pay_now" => "Confirme Seu Pedido",
         "vendor_amount" => "R$ do Vendedor",
         "admin_amount" => "R$ do iBench",
-    
+
         /* Marcello - Add Product */
         "select_item" => "Selecione",
         "product_orders" => "Lista de Pedidos",
-    
+
         /* Marcello - labels */
         "name_business" => "Nome da Empresa",
         "lbl_store" => "Empresa",
         "name_place" => "Area Designada ",
         "view_boleto" => "Visualizar Boleto",
-    
+
         /* Marcello - Wirecard */
         "connect_wirecard_title" => "Conecte sua conta Wirecard",
         "connect_wirecard_info" => "Clique para conectar iBench com Wirecard",
         "connect_wirecard" => "Conectar Wirecard",
-    
-	
-	
-	
+
+
+
+
 
 ];

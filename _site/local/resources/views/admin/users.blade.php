0022a58b81e4ca122182bece0ad2930147437c8a
marce <marce@DESKTOP-GBOIQLK>
2020-01-25 10:43:26 -0200

    Acerto de Layout e UTF8

--- _site/local/resources/views/admin/users.blade.php
+++ _site/local/resources/views/admin/users.blade.php
@@ -30,15 +30,15 @@
                {{ Session::get('success') }}
               </div>
     @endif
-<form action="{{ route('admin.users') }}" method="post">
+<form action="{{ route('admin.users') }}" method="post" accept-charset="utf-8">
 
                  {{ csrf_field() }}
-                 <div align="right">                  
+                 <div align="right">
                     <input type="submit" value="Deletar Todos" class="btn btn-danger" id="checkBtn" onClick="return confirm('Tem certeza que quer deletar?');">
                     <a href="<?php echo $url;?>/admin/adduser" class="btn btn-primary">Adicionar Usuario</a>
                     <a href="<?php echo $url;?>/admin/userlogs" class="btn btn-warning">Log Usuarios</a>
                  </div>
-                 
+
 <div class="widget-box">
 
           <div class="widget-title"> <span class="icon"><i class="icon-th"></i></span>
@@ -85,8 +85,8 @@
                          <td><img src="<?php echo $url.'/local/images/nophoto.jpg';?>" class="thumb" width="40" align="middle" style="display: block; margin-left: auto; margin-right: auto; z-index: 1;" >
                              </td>
                          <?php } ?>
-                             <td><?php echo utf8_decode() $user->name;?></td>
-                          <td><?php echo $user->full_name;?></td>
+                          <td><?php echo utf8_decode($user->name);?></td>
+                          <td><?php echo utf8_decode($user->full_name);?></td>
                           <td><?php echo $user->email;?></td>
                           <td><?php echo $user->cpf_cnpj;?></td>
 			                    <td><?php echo $user->phone;?></td>
@@ -104,14 +104,14 @@
                           <td>
                           <?php if($user->provider==""){?>
 			   <a href="<?php echo $url;?>/admin/edituser/{{ $user->id }}">
-                            <button type="button" class="btn btn-outline-info" alt="Editar" title="Editar"><i class="icon-fixed-width icon-pencil"></i></button>    
+                            <button type="button" class="btn btn-outline-info" alt="Editar" title="Editar"><i class="icon-fixed-width icon-pencil"></i></button>
                            </a>
-                  <?php } ?>			  
+                  <?php } ?>
 			@if($sta!=1)<a href="<?php echo $url;?>/admin/users/{{ $user->id }}" onClick="return confirm('Tem certeza que quer deletar?')">
                            <button type="button" class="btn btn-outline-danger" alt="Deletar" title="Deletar"><i class="icon-fixed-width icon-remove-circle"></i></button>
                         </a>
                         @endif
-                        
+
 			</td>
                         </tr>
                 <?php $i++; } } ?>
